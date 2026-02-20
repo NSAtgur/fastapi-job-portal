21c10f7b54a92daafbadccf65057955ed3a53625
NSAtgur <nsatgur24@gmail.com>
2026-02-20 12:08:33 +0530

    frontend fixed

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from routes import router
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def serve_frontend():
+    return FileResponse("static/index.html")
 app.include_router(router)
